86b1f300d88c954d192be476779a4eefff29fbee
GiuseppeCorrao <109970421+GiuseppeCorrao@users.noreply.github.com>
2022-08-25 12:25:40 +0200

    Delete src/products directory

--- src/products/software/VPNService.java
+++
@@ -1,8 +0,0 @@
-package products.software;
-
-public class VPNService extends Software{
-
-    public VPNService(String name, String description, String version, double price, String typeOfBit) {
-        super(name, description, version, price, typeOfBit);
-    }
-}
