41c20abb235d87486ba9d7f044d9310515407271
Samuele Catalano <csamuele73@mail.com>
2022-08-29 16:21:24 +0200

    refactor: refactoring constructors of Users and SubscriptionPlans

--- src/entities/Users.java
+++ src/entities/Users.java
@@ -17,18 +17,17 @@ public class Users extends FlowOpen {
     String password;
     List<Object> chart;
 
-    public Users(String name, String description, String[] contact, String address, Brand brand,String nameUser, String surnameUser,
-                 int age, LocalDate birthday, String sex, String email, String password, List<Object> chart) {
+    public Users(String name, String description, String[] contact, String address, Brand brand, String nameUser, String surnameUser, int age, LocalDate birthday, String sex, String email, String password, List<Object> chart) {
         super(name, description, contact, address, brand);
         this.nameUser = nameUser;
         this.surnameUser = surnameUser;
+        this.age = age;
         this.birthday = birthday;
         this.sex = sex;
         this.email = email;
         this.password = password;
         this.chart = chart;
     }
-
 
     void signIn(){};
     void login(){};
