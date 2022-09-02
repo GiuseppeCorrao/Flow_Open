2dd8b17129881275fab3232fd0390f61c999846d
GiuseppeCorrao <109970421+GiuseppeCorrao@users.noreply.github.com>
2022-09-02 14:54:35 +0200

    fix: remove variables

--- src/entities/Employees.java
+++ src/entities/Employees.java
@@ -1,7 +1,5 @@
 package entities;
 
-import motherclass.FlowOpen;
-import products.Brand;
 import workinglogic.WorkingDays;
 
 public class Employees {
@@ -10,8 +8,7 @@ public class Employees {
     String surnameEmployee;
     WorkingDays workingDays;
 
-    public Employees(String name, String description, String[] contact, String address, Brand brand, String nameEmployee,
-                     String surname, WorkingDays workingDays) {
+    public Employees(String nameEmployee,String surname, WorkingDays workingDays) {
         this.nameEmployee = nameEmployee;
         this.surnameEmployee = surnameEmployee;
         this.workingDays = workingDays;
