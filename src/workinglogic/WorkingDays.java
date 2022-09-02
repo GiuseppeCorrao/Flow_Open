27138538717bc050f78ba5d26f918d5077c9b962
GiuseppeCorrao <109970421+GiuseppeCorrao@users.noreply.github.com>
2022-09-02 14:59:09 +0200

    fix: remove variables

--- src/workinglogic/WorkingDays.java
+++ src/workinglogic/WorkingDays.java
@@ -1,8 +1,5 @@
 package workinglogic;
 
-import motherclass.FlowOpen;
-import products.Brand;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -13,8 +10,7 @@ public class WorkingDays{
     DAYS days;
 
 
-    public WorkingDays(String name,String description,String[] contact,String address,Brand brand, LocalDate workingDays,
-                       LocalDateTime workingHours, DAYS days) {
+    public WorkingDays(LocalDate workingDays,LocalDateTime workingHours, DAYS days) {
         this.workingDays = workingDays;
         this.workingHours = workingHours;
         this.days = days;
