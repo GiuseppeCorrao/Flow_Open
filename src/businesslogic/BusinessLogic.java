fe3a4f6e3c4ecefea7d7d1c0d402cba67f5b4965
Samuele Catalano <csamuele73@mail.com>
2022-09-02 12:34:48 +0200

    fix: BusinessLogic

--- src/businesslogic/BusinessLogic.java
+++ src/businesslogic/BusinessLogic.java
@@ -1,14 +1,7 @@
 package businesslogic;
 
-import motherclass.FlowOpen;
-import products.Brand;
 
-public class BusinessLogic extends FlowOpen {
-
-    public BusinessLogic(String name, String description, String[] contact, String address, Brand brand) {
-        super(name, description, contact, address, brand);
-    }
-
+public class BusinessLogic {
 
 
     public static double calculateIva(double price) {
