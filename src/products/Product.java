c44fe3d4bea6a1f468fd08fb56fd2f76e021e5f7
Giuseppe Corrao <josephcorr99@gmail.com>
2022-09-02 11:14:18 +0200

    refactor: refactored Product class and relative inheritance

--- src/products/Product.java
+++ src/products/Product.java
@@ -2,20 +2,77 @@ package products;
 
 public class Product {
 
+    /**
+     * @author Giuseppe Corrao
+     * @version 2.0
+     * this class has been refactor + add getter and setter
+     */
+
     private Brand brand;
     private String model;
     private String color;
     private double price;
     private String description;
 
+    private String product;
 
-    public Product(Brand brand, String model, String color, double price, String description) {
+
+    public Product(Brand brand, String model, String color, double price, String description,String product) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.price = price;
         this.description = description;
+        this.product = product;
 
 
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
     }
 }
