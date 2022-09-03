db56c1bcaca29ceb48038d85797a2d9351f1e09f
sow mjt <Sow@sow.sow>
2022-09-03 18:23:05 +0200

    change and insert login, signin and SubscriptionPlans

--- src/businesslogic/SubscriptionPlans.java
+++ src/businesslogic/SubscriptionPlans.java
@@ -8,15 +8,25 @@ import java.util.List;
 
 public class SubscriptionPlans extends Users {
 
-    double price;
+    public double price;
 
-    public SubscriptionPlans(String name, String description, String[] contact, String address, Brand brand, String nameUser, String surnameUser, int age, LocalDate birthday, String sex, String email, String password,double price) {
-        super(name, description, contact, address, brand, nameUser, surnameUser, age, birthday, sex, email, password);
-        this.price = price;
+    SubscriptionPlans (){}
+
+    public SubscriptionPlans(String name, String description, String[] contact, String address, Brand brand, String nameUser,
+                             String surnameUser, int age, LocalDate birthday, String sex, String email, String password, List<Object> chart) {
+
     }
 
-    void subscriptionCheck() {
-    }
+    /**
+     * @param users of user subscription plans
+     * this method check if the user is subscripted
+     */
+    public void subscriptionCheck(Users users) {
 
+        if (users.getNameUser() != null && users.getSurnameUser() != null && users.getEmail() != null && users.getSex()
+                != null && users.getBirthday() != null && users.getPassword() != null) {
+            System.out.println("subscription corrected");
+        }
+    }
 
 }
--- src/entities/Users.java
+++ src/entities/Users.java
@@ -5,45 +5,113 @@ import java.util.List;
 
 public class Users{
 
-    String nameUser;
-    String surnameUser;
-    int age;
-    LocalDate birthday;
-    String sex;
-    String email;
-    String password;
+    private String nameUser;
+    private String surnameUser;
+    private int age;
+    private LocalDate birthday;
+    private String sex;
+    private String email;
+    private String password;
 
+    public Users() {
+
+    }
 
     public Users(String nameUser, String surnameUser, int age, LocalDate birthday, String sex, String email, String password) {
         this.nameUser = nameUser;
         this.surnameUser = surnameUser;
-        this.age = age;
         this.birthday = birthday;
         this.sex = sex;
         this.email = email;
         this.password = password;
+        this.age = age;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getSurnameUser() {
+        return surnameUser;
+    }
+
+    public void setSurnameUser(String surnameUser) {
+        this.surnameUser = surnameUser;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * this method is when the user want signup
+     */
+    public void signIn() {
+
+        new Users();
+
+        nameUser = getNameUser();
+        surnameUser = getSurnameUser();
+        sex = getSex();
+        email = getEmail();
+        birthday = getBirthday();
+        password = getPassword();
+        System.out.println("nameUser: " + nameUser + "\n" + "surname : " + surnameUser + "\n" + "sex: " + sex +
+                "\n" + "email: " + email + "\n" + "birthday: " + birthday + "\n" + "password: " + "*".repeat(password.length()));
 
     }
 
-    void signIn() {
-    }
 
-    ;
-
+    /**
+     * when the user want to loggin
+     */
     void login() {
+
+        if (this.email.equals(getEmail()) && this.password.equals(getPassword())) {
+            System.out.println("you are loggin");
+        } else {
+            System.out.println("the dates is incorrect, please try");
+        }
+
     }
-
-    ;
-
-    void totalPrice() {
-    }
-
-    ;
-
-    void sum() {
-    }
-
-    ;
-
-
 }
