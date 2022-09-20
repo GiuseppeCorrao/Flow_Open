bbf2764eb71cd2bd699d1920241dcf0697bd52c5
Emmanuele La Duca <laducaemanuele20@gmail.com>
2022-09-20 18:29:50 +0200

    fix: add_function_Discount

--- src/businesslogic/BusinessLogic.java
+++ src/businesslogic/BusinessLogic.java
@@ -1,12 +1,21 @@
 package businesslogic;
 
 
+import products.Product;
+
 public class BusinessLogic {
 
 
     public static double calculateIva(double price) {
         int iva = 22;
         return (price * iva) / 100;
+    }
+
+    public double getCalculateDiscount(Product product){
+        int discountPercentage= 23;
+        double priceDiscount= (discountPercentage * product.getPrice())/100;
+        priceDiscount= product.getPrice() - priceDiscount;
+        return priceDiscount;
     }
 
 }
--- test/BusinessLogicTest.java
+++ test/BusinessLogicTest.java
@@ -1,11 +1,21 @@
 import businesslogic.BusinessLogic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import products.Brand;
+import products.Product;
 
 public class BusinessLogicTest {
 
     @Test
     void calculateIvaTest() {
         Assertions.assertEquals(73.26, BusinessLogic.calculateIva(333));
+    }
+
+
+    @Test
+    void calculateDiscountTest(){
+        BusinessLogic product= new BusinessLogic();
+        Product product1= new Product(Brand.APPLE, "4x4", "rosso", 135.00, "è un pc molto veloce, potente", "computer", 50);
+        Assertions.assertEquals(103.95, product.getCalculateDiscount(product1));
     }
 }
