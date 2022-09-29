c6a8d95403896f0578413af6a0ef23eecb92e313
Samuele Catalano <csamuele73@mail.com>
2022-09-29 17:32:46 +0200

    feat: obtained user from database

--- SQL_command/CreateTable.sql
+++ SQL_command/CreateTable.sql
@@ -2,4 +2,16 @@ CREATE TABLE IF NOT EXISTS Product(
 product_id INTEGER(100) NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(30),
 product_price INTEGER(300),
-PRIMARY KEY (product_id);
+PRIMARY KEY (product_id));
+
+CREATE TABLE IF NOT EXISTS User(
+User_id INTEGER(100) NOT NULL AUTO_INCREMENT,
+User_name VARCHAR(30),
+User_surname VARCHAR(255),
+User_age INTEGER(120),
+User_birthday DATE,
+User_sex ENUM('male', 'female'),
+User_email VARCHAR(50),
+User_password VARCHAR(30),
+PRIMARY KEY (User_id));
+)
--- src/entities/User.java
+++ src/entities/User.java
@@ -1,15 +1,18 @@
 package entities;
-import java.time.LocalDate;
+
+import java.sql.*;
+import java.util.List;
 
 public class User {
 
     String nameUser;
     String surnameUser;
     int age;
-    LocalDate birthday;
+    Date birthday;
     String sex;
     String email;
     String password;
+    List<String> userObject;
 
     public User(String email, String password) {
         this.email = email;
@@ -17,7 +20,7 @@ public class User {
 
     }
 
-    public User(String nameUser, String surnameUser, int age, LocalDate birthday, String sex, String email, String password) {
+    public User(String nameUser, String surnameUser, int age, Date birthday, String sex, String email, String password) {
         this.nameUser = nameUser;
         this.surnameUser = surnameUser;
         this.age = age;
@@ -26,6 +29,36 @@ public class User {
         this.email = email;
         this.password = password;
 
+    }
+
+    public List<User> takeUserObjects(List<User> userObject) {
+
+        Connection conn;
+
+        try {
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/flowopenDev", "developer", "developer1_");
+
+            Statement statement = conn.createStatement();
+
+            ResultSet pointer = statement.executeQuery("SELECT * FROM User");
+
+            while (pointer.next()) {
+                nameUser = pointer.getString("User_name");
+                surnameUser = pointer.getString("User_surname");
+                age = pointer.getInt("User_age");
+                birthday = pointer.getDate("User_birthday");
+                sex = pointer.getString("User_sex");
+                email = pointer.getString("User_email");
+                password = pointer.getString("User_password");
+
+                userObject.add(new User(nameUser, surnameUser, age, birthday, sex, email, password));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return userObject;
     }
 }
 
